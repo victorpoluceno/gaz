17958acca35d7504a13222cdd860c69f027762cc
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2013-09-18 00:50:54 -0300

    Latest sync with mvp spec

--- gaz/gaz/product/models.py
+++ gaz/gaz/product/models.py
@@ -2,15 +2,23 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=30, unique=True)
+    cnpj = models.CharField(max_length=30)
+    name = models.CharField(max_length=200)
+    corporate_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
 
 
 class Deposit(models.Model):
+    code = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=30)
+    company = models.ForeignKey(Company)
 
 
-class MeasurementUnit(models.Model):
-    name = models.CharField(max_length=30)
+class Tank(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=200)
+    company = models.ForeignKey(Company)
 
 
 class ProductGroup(models.Model):
@@ -18,36 +26,29 @@ class ProductGroup(models.Model):
 
 
 class Product(models.Model):
-    code = models.CharField(max_length=30)
+    code = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=200)
-    company = models.ForeignKey(Company, blank=True)
     deposits = models.ManyToManyField(Deposit)
     group = models.ForeignKey(ProductGroup)
     price = models.FloatField()
-    unit = models.ForeignKey(MeasurementUnit)
-
-
-class ProductBarcode(models.Model):
-    code = models.CharField(max_length=30)
-    product = models.ForeignKey(Product)
+    barcode = models.CharField(max_length=30)
 
 
 class Fuel(models.Model):
     code = models.CharField(max_length=30, unique=True)
     code_anp = models.CharField(max_length=30)
     name = models.CharField(max_length=200)
-    unit = models.ForeignKey(MeasurementUnit)
     price = models.FloatField()
+    tank = models.ForeignKey(Tank)
 
 
-class Tank(models.Model):
+class Pump(models.Model):
     code = models.CharField(max_length=30, unique=True)
-    company = models.ForeignKey(Company, blank=True)
-    description = models.CharField(max_length=30)
-    fuel = models.ForeignKey(Fuel)
+    name = models.CharField(max_length=200)
 
 
-class Bico(models.Model):
+class Nozzle(models.Model):
     code = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=200)
     tank = models.ForeignKey(Tank)
+    pump = models.ForeignKey(Pump)
